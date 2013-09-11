2c9e0cb485e7172c59621377b9ede299fa97e0e5
Neal Wu <neal@nealwu.com>
2013-09-11 00:15:31 -0400

    Started working on interp

--- ps0/mips_sim.ml
+++ ps0/mips_sim.ml
@@ -37,20 +37,20 @@ let int32_to_int16 (num : int32) : int =
 
 let to_i32 (inst : inst) : int32 =
   match inst with
-    Add (r1, r2, r3) -> Int32.of_int ((((0 lsl 5 + reg2ind r2) lsl 5 + reg2ind r3) lsl 5 + reg2ind r1) lsl 11 + 0x20)
-  | Beq (r1, r2, i1) -> Int32.of_int (((4 lsl 5 + reg2ind r1) lsl 5 + reg2ind r2) lsl 16 + int32_to_int16 i1)
-  | Jr (r1) -> Int32.of_int ((0 lsl 5 + reg2ind r1) lsl 21 + 8)
-  | Jal (i1) -> Int32.of_int (3 lsl 26 + Int32.to_int i1)
+    Add (r1, r2, r3) -> Int32.of_int ((((0x0 lsl 5 + reg2ind r2) lsl 5 + reg2ind r3) lsl 5 + reg2ind r1) lsl 11 + 0x20)
+  | Beq (r1, r2, i1) -> Int32.of_int (((0x4 lsl 5 + reg2ind r1) lsl 5 + reg2ind r2) lsl 16 + int32_to_int16 i1)
+  | Jr (r1) -> Int32.of_int ((0x0 lsl 5 + reg2ind r1) lsl 21 + 0x8)
+  | Jal (i1) -> Int32.of_int (0x3 lsl 26 + Int32.to_int i1)
   | Li (r1, i1) -> raise FatalError
-  | Lui (r1, i1) -> Int32.of_int ((15 lsl 10 + reg2ind r1) lsl 16 + int32_to_int16 i1)
-  | Ori (r1, r2, i1) -> Int32.of_int (((13 lsl 5 + reg2ind r2) lsl 5 + reg2ind r1) lsl 16 + int32_to_int16 i1)
-  | Lw (r1, r2, i1) -> Int32.of_int (((35 lsl 5 + reg2ind r2) lsl 5 + reg2ind r1) lsl 16 + int32_to_int16 i1)
-  | Sw (r1, r2, i1) -> Int32.of_int (((43 lsl 5 + reg2ind r2) lsl 5 + reg2ind r1) lsl 16 + int32_to_int16 i1)
+  | Lui (r1, i1) -> Int32.of_int ((0xF lsl 10 + reg2ind r1) lsl 16 + int32_to_int16 i1)
+  | Ori (r1, r2, i1) -> Int32.of_int (((0xD lsl 5 + reg2ind r2) lsl 5 + reg2ind r1) lsl 16 + int32_to_int16 i1)
+  | Lw (r1, r2, i1) -> Int32.of_int (((0x23 lsl 5 + reg2ind r2) lsl 5 + reg2ind r1) lsl 16 + int32_to_int16 i1)
+  | Sw (r1, r2, i1) -> Int32.of_int (((0x2B lsl 5 + reg2ind r2) lsl 5 + reg2ind r1) lsl 16 + int32_to_int16 i1)
 
-let rec load_memory (word : int32) (pc : int32) (mem: memory) : memory =
+let rec store_memory (word : int32) (pc : int32) (mem : memory) : memory =
   if Int32.compare word Int32.zero == 0 then mem else
     let new_mem = mem_update pc (mk_byte word) mem in
-      load_memory (Int32.shift_right_logical word 8) (Int32.succ pc) new_mem
+      store_memory (Int32.shift_right_logical word 8) (Int32.succ pc) new_mem
 
 (* Map a program, a list of Mips assembly instructions, down to a starting 
    state. You can start the PC at any address you wish. Just make sure that 
@@ -62,15 +62,47 @@ let rec assem (prog : program) : state =
       | inst::ls -> (
           match inst with
             | Li (r1, i1) ->
-              let mem = load_memory (to_i32 (Lui(r1, (Int32.shift_right_logical i1 16)))) mem_loc mem in
-                let mem = load_memory (to_i32 (Ori(r1, R0, Int32.logand 0x0000FFFFl i1))) (Int32.add mem_loc 4l) mem in
+              let mem = store_memory (to_i32 (Lui (r1, (Int32.shift_right_logical i1 16)))) mem_loc mem in
+                let mem = store_memory (to_i32 (Ori (r1, R0, Int32.logand 0x0000FFFFl i1))) (Int32.add mem_loc 4l) mem in
                   assem_helper ls (Int32.add mem_loc 8l) mem
-            | _ -> assem_helper ls (Int32.add mem_loc 4l) (load_memory (to_i32 inst) mem_loc mem)
+            | _ -> assem_helper ls (Int32.add mem_loc 4l) (store_memory (to_i32 inst) mem_loc mem)
           )
       | [] -> mem
   in let loaded_mem = assem_helper prog Int32.zero empty_mem in
     {r = empty_rf; pc = Int32.zero; m = loaded_mem}
 
-
 (* Given a starting state, simulate the Mips machine code to get a final state *)
-let rec interp (init_state : state) : state = init_state
+let rec interp (init_state : state) : state =
+  let mem = state.m in
+  let load_word (pc : int32) : int =
+    let d = Int32.to_int (b2i32 (mem_lookup pc mem)) in
+    let c = Int32.to_int (b2i32 (mem_lookup (Int32.add pc 1l) mem)) in
+    let b = Int32.to_int (b2i32 (mem_lookup (Int32.add pc 2l) mem)) in
+    let a = Int32.to_int (b2i32 (mem_lookup (Int32.add pc 3l) mem))in
+    a lsl 24 + b lsl 16 + c lsl 8 + d
+  in
+  let word = load_word state.pc in
+  if word == 0 then init_state else
+  let first_six (word : int) : int =
+    (word land 0xFC) lsr 26
+  in
+  let last_six (word: int) : int =
+    word land 0x3F
+  in
+  if first_six word == 0x0 and last_six word == 0x20 then
+    (* Add *)
+  else if first_six word == 0x4 then
+    (* Beq *)
+  else if first_six word == 0x0 and last_six word == 0x8 then
+    (* Jr *)
+  else if first_six word == 0x3 then
+    (* Jal *)
+  else if first_six word == 0xF then
+    (* Lui *)
+  else if first_six word == 0xD then
+    (* Ori *)
+  else if first_six word == 0x23 then
+    (* Lw *)
+  else if first_six word == 0x2B then
+    (* Sw *)
+  else raise FatalError
