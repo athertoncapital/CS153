be49717634224807958f1b365364cdbdc663a514
Tianen Li <tianen@Tianens-MacBook-Pro.local>
2013-09-25 20:52:24 -0400

    binop parsing works

--- ps1/combparser.ml
+++ ps1/combparser.ml
@@ -6,20 +6,34 @@ open Ast
 
 let dummy_pos : pos = 0
 
-let rec make_exp_parser (():unit) : (token, exp) parser =
+let binop_map = map (fun (e1, (op, e2)) -> (Binop (e1, op, e2), dummy_pos))
+
+let make_next_binop_parser parser op = 
+  let rec next_parser () =
+    let op_parser = satisfy_opt op in
+    let op_exp_parser = binop_map (lazy_seq (lazy (parser()), lazy (seq(op_parser, next_parser())))) in
+      alt (parser(), op_exp_parser)
+  in next_parser ()
+
+let rec make_aexp_parser() =
   let int_parser = satisfy_opt (function INT i -> Some (Int i, dummy_pos) | _ -> None) in
   let sub_parser = seq (satisfy (fun t -> t == LPAREN), 
     lazy_seq (lazy (make_exp_parser ()), lazy (satisfy (fun t -> t == RPAREN)))) in 
   let sub_exp_parser = map (fun (_, (e, _)) -> e) sub_parser in
-  let first_parser = alt (int_parser, sub_exp_parser) in
-  let rest_parser = seq (first_parser, make_binop_rest ()) in
-  let binop_parser = map (fun (e1, (op, e2)) -> (Binop (e1, op, e2), dummy_pos)) rest_parser in
-  alts [binop_parser; first_parser]
+  alt (int_parser, sub_exp_parser)
 
-and make_binop_rest (():unit) : (token, (binop * exp)) parser =
-  let binop_op_parser = satisfy_opt (function 
-    PLUS -> Some Plus | MINUS -> Some Minus | STAR -> Some Times | SLASH -> Some Div | _ -> None) in
-  lazy_seq (lazy binop_op_parser, lazy (make_exp_parser ()))
+and make_bexp_parser () = 
+  make_next_binop_parser (make_aexp_parser) (function STAR -> Some Times | SLASH -> Some Div | _ -> None)
+
+and make_cexp_parser () = 
+  make_next_binop_parser (make_bexp_parser) (function PLUS -> Some Plus | MINUS -> Some Minus | _ -> None)
+
+and make_dexp_parser () = 
+  make_next_binop_parser (make_cexp_parser) (function
+    EQ -> Some Eq | NEQ -> Some Neq | LT -> Some Lt | LTE -> Some Lte | GT -> Some Gt | GTE -> Some Gte | _ -> None)
+
+and make_exp_parser () = 
+  make_dexp_parser()
 
 let rec make_stmt_parser (():unit) : (token, stmt) parser =
   let token_p tk = satisfy (fun t -> t == tk) in
