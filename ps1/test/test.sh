b38e9935809a6d1640fa4a94294382fe8add1ca3
Neal Wu <neal@nealwu.com>
2013-09-25 22:50:14 -0400

    Don't test test.sh in test.sh

--- ps1/test/test.sh
+++ ps1/test/test.sh
@@ -4,6 +4,6 @@ cd ..
 make all
 cd test
 
-for file in `ls`; do echo $file; ../ps1comb $file; done
+for file in `ls | grep -v test.sh`; do echo $file; ../ps1comb $file; done
 
 # for i in $(seq -f "%02g" 1 16); do file=$(find . -name '01cexpr_'$i'*'); echo $i; echo $file; ../ps1comb $file; done
