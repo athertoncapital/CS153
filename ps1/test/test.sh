475ed89555929aa27c39f0e7b065d9e99f4263ad
Neal Wu <neal@nealwu.com>
2013-09-25 22:01:01 -0400

    Test all files now

--- ps1/test/test.sh
+++ ps1/test/test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-for i in $(seq -f "%02g" 1 16); do file=$(find . -name '01cexpr_'$i'*'); echo $i; echo $file; ../ps1comb $file; done
+cd ..
+make all
+cd test
+
+for file in `ls`; do echo $file; ../ps1comb $file; done
+
+# for i in $(seq -f "%02g" 1 16); do file=$(find . -name '01cexpr_'$i'*'); echo $i; echo $file; ../ps1comb $file; done
