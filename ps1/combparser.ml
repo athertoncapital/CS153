be94fe99be58b98379c20822ef5b5860189224c9
Tianen Li <tianen@Tianens-MacBook-Pro.local>
2013-09-25 21:18:52 -0400

    parser almost done, need to support negation of ints

--- ps1/combparser.ml
+++ ps1/combparser.ml
@@ -8,19 +8,22 @@ let dummy_pos : pos = 0
 
 let binop_map = map (fun (e1, (op, e2)) -> (Binop (e1, op, e2), dummy_pos))
 
-let make_next_binop_parser parser op = 
+let make_next_parser mapping parser op = 
   let rec next_parser () =
     let op_parser = satisfy_opt op in
-    let op_exp_parser = binop_map (lazy_seq (lazy (parser()), lazy (seq(op_parser, next_parser())))) in
+    let op_exp_parser = mapping (lazy_seq (lazy (parser()), lazy (seq(op_parser, next_parser())))) in
       alt (parser(), op_exp_parser)
   in next_parser ()
+
+let make_next_binop_parser = make_next_parser binop_map
 
 let rec make_aexp_parser() =
   let int_parser = satisfy_opt (function INT i -> Some (Int i, dummy_pos) | _ -> None) in
   let sub_parser = seq (satisfy (fun t -> t == LPAREN), 
     lazy_seq (lazy (make_exp_parser ()), lazy (satisfy (fun t -> t == RPAREN)))) in 
   let sub_exp_parser = map (fun (_, (e, _)) -> e) sub_parser in
-  alt (int_parser, sub_exp_parser)
+  let var_parser = satisfy_opt (function ID v -> Some (Var v, dummy_pos) | _ -> None) in
+  alts [int_parser; var_parser; sub_exp_parser]
 
 and make_bexp_parser () = 
   make_next_binop_parser (make_aexp_parser) (function STAR -> Some Times | SLASH -> Some Div | _ -> None)
@@ -32,8 +35,26 @@ and make_dexp_parser () =
   make_next_binop_parser (make_cexp_parser) (function
     EQ -> Some Eq | NEQ -> Some Neq | LT -> Some Lt | LTE -> Some Lte | GT -> Some Gt | GTE -> Some Gte | _ -> None)
 
+and make_eexp_parser () =
+  let not_parser = satisfy_opt (function NOT -> Some 1 | _ -> None) in
+  let not_mapping = fun (_, e) -> (Not e, dummy_pos) in
+  let not_exp_parser = map (not_mapping) (lazy_seq(lazy not_parser, lazy (make_eexp_parser()))) in
+  alt (make_dexp_parser(), not_exp_parser)
+
+and make_fexp_parser () =
+  let and_mapping = map (fun (e1, (_, e2)) -> (And (e1, e2), dummy_pos)) in
+  make_next_parser (and_mapping) (make_eexp_parser) (function AND -> Some 1 | _ -> None)
+
+and make_gexp_parser () =
+  let or_mapping = map (fun (e1, (_, e2)) -> (Or (e1, e2), dummy_pos)) in
+  make_next_parser (or_mapping) (make_fexp_parser) (function OR -> Some 1 | _ -> None)
+
 and make_exp_parser () = 
-  make_dexp_parser()
+  let var_parser = satisfy_opt (function ID v -> Some v | _ -> None) in
+  let eq_parser = satisfy_opt (function ASSIGN -> Some 1 | _ -> None) in
+  let assign_parser = seq(var_parser, lazy_seq(lazy eq_parser, lazy (make_gexp_parser()))) in
+  let assign_exp_parser = map (fun (v, (_, e)) -> (Assign (v, e), dummy_pos)) assign_parser in
+  alt (make_gexp_parser(), assign_exp_parser)
 
 let rec make_stmt_parser (():unit) : (token, stmt) parser =
   let token_p tk = satisfy (fun t -> t == tk) in
