7427ee8db78dee836a4adffc68bcb321f112bf7b
Tianen Li <tli516@gmail.com>
2013-10-31 23:11:15 -0400

    implemented mlish_compile

--- ps5/mlish_compile.ml
+++ ps5/mlish_compile.ml
@@ -2,4 +2,41 @@ module ML = Mlish_ast
 module S = Scish_ast
 
 exception ImplementMe
-let rec compile_exp ((e,_):ML.exp) : S.exp = raise ImplementMe
+exception FatalError
+
+open S
+
+let primop_of (p : ML.prim) : primop =
+  match p with
+  | ML.Plus -> Plus
+  | ML.Minus -> Minus
+  | ML.Times -> Times
+  | ML.Div -> Div
+  | ML.Eq -> Eq
+  | ML.Lt -> Lt
+  | ML.Pair -> Cons
+  | ML.Fst -> Fst
+  | ML.Snd -> Snd
+  | _ -> raise FatalError
+
+let rec compile_exp ((e,_):ML.exp) : S.exp = 
+  match e with
+  | ML.Var v -> Var v
+  | ML.PrimApp (p, es) -> compile_prim_exp p es
+  | ML.Fn (v, e) -> Lambda (v, compile_exp e)
+  | ML.App (e1, e2) -> App (compile_exp e1, compile_exp e2)
+  | ML.If (e1, e2, e3) -> If (compile_exp e1, compile_exp e2, compile_exp e3)
+  | ML.Let (v, e1, e2) -> sLet v (compile_exp e1) (compile_exp e2)
+
+and compile_prim_exp (p: ML.prim) (es: ML.exp list) : S.exp =
+  let es = List.map compile_exp es in
+  match p with
+  | ML.Int i -> Int i
+  | ML.Bool b -> if b then Int 1 else Int 0
+  | ML.Nil -> PrimApp(Cons, [Int 1; Int 0])
+  | ML.IsNil -> PrimApp(Fst, es)
+  | ML.Cons -> PrimApp(Cons, [Int 0; PrimApp(Cons, es)])
+  | ML.Hd -> PrimApp(Fst, [PrimApp(Snd, es)])
+  | ML.Tl -> PrimApp(Snd, [PrimApp(Snd, es)])
+  | _ -> PrimApp(primop_of p, es)
+
