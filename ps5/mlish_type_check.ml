eda24f10a4638a48d9dcd3d669761f22a1a5bd2e
Tianen Li <tli516@gmail.com>
2013-10-31 20:12:21 -0400

    generalize done, moved some functions around

--- ps5/mlish_type_check.ml
+++ ps5/mlish_type_check.ml
@@ -1,19 +1,44 @@
 open Mlish_ast
 
 exception TypeError
+exception NotFound
+
 let type_error(s:string) = (print_string s; raise TypeError)
+
+module GuessSet = Set.Make(struct
+                           type t = tipe
+                           let compare x y = if (x == y) then 0 else if (x = y) then 1 else compare x y
+                         end)
 
 type environment = (var * tipe_scheme) list
 
 let guess() = Guess_t(ref None)
+
+let var_counter = ref 0
+
+let fresh_var() = let x = !var_counter in
+  var_counter:= x + 1; "t" ^ string_of_int(x)
 
 let rec lookup ls key =
   match ls with
   | (k, v)::tl -> if k = key then v else lookup tl key
   | _ -> raise TypeError
 
+let rec lookup_guess ls key =
+  match ls with
+  | (k, v)::tl -> if k == key then v else lookup_guess tl key
+  | _ -> raise NotFound
+
 let extend (env: environment) (v: var) (ts: tipe_scheme) =
   (v, ts)::env
+
+let rec guesses_of_tipe (t: tipe) : GuessSet.t =
+  match t with
+  | Fn_t (t1, t2) -> GuessSet.union (guesses_of_tipe t1) (guesses_of_tipe t2)
+  | Pair_t (t1, t2) -> GuessSet.union (guesses_of_tipe t1) (guesses_of_tipe t2)
+  | List_t t1 -> guesses_of_tipe t1
+  | Guess_t _ -> GuessSet.singleton t
+  | _ -> GuessSet.empty
 
 let rec substitute (tvars: (tvar * tipe) list) (t:tipe) : tipe =
   match t with
@@ -26,10 +51,51 @@ let rec substitute (tvars: (tvar * tipe) list) (t:tipe) : tipe =
                   | None -> t)
   | _ -> t
 
+let rec substitute_guesses (gs: (tipe * tvar) list) (t:tipe) =
+  match t with
+  | Fn_t (t1, t2) -> Fn_t ((substitute_guesses gs t1), (substitute_guesses gs t2))
+  | Pair_t (t1, t2) -> Pair_t ((substitute_guesses gs t1), (substitute_guesses gs t2))
+  | List_t t1 -> List_t (substitute_guesses gs t1)
+  | Guess_t _ -> (try Tvar_t(lookup gs t) with NotFound -> t)
+  | _ -> t
+
+let rec occurs (opt:tipe option ref) (t:tipe) : bool =
+  match t with
+  | Fn_t (t1, t2) -> occurs opt t1 || occurs opt t2
+  | Pair_t (t1, t2) -> occurs opt t1 || occurs opt t2
+  | List_t t1 -> occurs opt t1
+  | Guess_t opt2 -> 
+      if (opt = opt2) then true
+      else (match !opt2 with
+            | Some t1 -> occurs opt t1
+            | None -> false)
+  | _ -> false
+
 let instantiate (ts: tipe_scheme) : tipe =
   let Forall(vs, t) = ts in
     let vs_and_ts = List.map (fun a -> (a, guess())) vs in
     substitute vs_and_ts t
+
+let rec unify (t1: tipe) (t2: tipe) : bool =
+  if t1 == t2 then true else
+  match t1, t2 with
+  | Guess_t r, _ -> (match !r with 
+                        | Some t1' -> unify t1' t2
+                        | None -> if occurs r t2 then raise TypeError else (r := Some t2; true))
+  | _, Guess_t _ -> unify t2 t1
+  | Fn_t (a, b) , Fn_t (c, d) -> (unify a c) && (unify b d)
+  | Pair_t (a, b), Pair_t (c, d) -> (unify a c) && (unify b d)
+  | List_t a, List_t b -> unify a b
+  | _ -> (t1 = t2)
+
+let generalize (env: environment) (t: tipe) : tipe_scheme =
+  let t_gs = guesses_of_tipe t in
+  let env_list_gs = List.map (function (x, Forall(vs, t)) -> guesses_of_tipe t) env in
+  let env_gs = List.fold_left GuessSet.union GuessSet.empty env_list_gs in
+  let diff = GuessSet.elements (GuessSet.diff t_gs env_gs) in
+  let gs_vs = List.map (fun g -> (g, fresh_var())) diff in
+  let ts = substitute_guesses gs_vs t in
+  Forall(List.map snd gs_vs, ts)
 
 let rec tc (env: environment) ((e, _): exp) : tipe =
   match e with
@@ -79,33 +145,6 @@ and check_prims (env: environment) (p: prim) (es: exp list) : tipe =
 and check_binop env e1 e2 expected_t1 expected_t2 tout =
   let (t1, t2) = ((tc env e1), (tc env e2)) in
     if (unify t1 expected_t1) && (unify t2 expected_t2) then tout else raise TypeError
-
-and unify (t1: tipe) (t2: tipe) : bool =
-  if t1 == t2 then true else
-  match t1, t2 with
-  | Guess_t r, _ -> (match !r with 
-                        | Some t1' -> unify t1' t2
-                        | None -> if occurs r t2 then raise TypeError else (r := Some t2; true))
-  | _, Guess_t _ -> unify t2 t1
-  | Fn_t (a, b) , Fn_t (c, d) -> (unify a c) && (unify b d)
-  | Pair_t (a, b), Pair_t (c, d) -> (unify a c) && (unify b d)
-  | List_t a, List_t b -> unify a b
-  | _ -> false
-
-and occurs (opt:tipe option ref) (t:tipe) : bool =
-  match t with
-  | Fn_t (t1, t2) -> occurs opt t1 || occurs opt t2
-  | Pair_t (t1, t2) -> occurs opt t1 || occurs opt t2
-  | List_t t1 -> occurs opt t1
-  | Guess_t opt2 -> 
-      if (opt = opt2) then true
-      else (match !opt2 with
-            | Some t1 -> occurs opt t1
-            | None -> false)
-  | _ -> false
-
-and generalize (env: environment) (t: tipe) : tipe_scheme =
-  raise TypeError
 
 let type_check_exp (e:Mlish_ast.exp) : tipe = 
   tc [] e
