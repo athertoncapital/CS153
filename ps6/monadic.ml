d8fe8354cf5aab7c4f765e72507874094c0e0a98
Neal Wu <neal@nealwu.com>
2013-11-12 22:54:04 -0500

    Style changes

--- ps6/monadic.ml
+++ ps6/monadic.ml
@@ -361,14 +361,13 @@ let never_inline_thresh  (e: exp) : bool = false (** Never inline  **)
 let rec size_val v =
   match v with
   | Op _ -> 1
-  | PrimApp (_, ls) -> 1 + (List.length ls)
-  | Lambda (_, e) -> 1 + (size_exp e)
-  
+  | PrimApp (_, ls) -> 1 + List.length ls
+  | Lambda (_, e) -> 1 + size_exp e
 and size_exp e = 
   match e with
   | Return _ -> 2
   | LetVal (_, v, e) -> 1 + (size_val v) + (size_exp e)
-  | LetCall (_, _, _, e) -> 3 + (size_exp e)
+  | LetCall (_, _, _, e) -> 3 + size_exp e
   | LetIf (_, _, e1, e2, e3) -> 2 + (size_exp e1) + (size_exp e2) + (size_exp e3)
 
 (* return true if the expression e is smaller than i, i.e. it has fewer
